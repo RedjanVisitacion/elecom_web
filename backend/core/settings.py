be15c120cc0cad73dea8447b407af19874f6b30b
RedjanVisitacion <visitacionredjan7@gmail.com>
2026-05-16 23:46:43 +0800

    UPDATED

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -160,8 +160,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/elecom_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
