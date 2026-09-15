48fb8f2d3f7734ad82e21cc829102e83d2cd2205
RedjanVisitacion <visitacionredjan7@gmail.com>
2026-09-15 08:14:51 +0800

    UPDATED

--- backend/backup/fix_admin_password.sql
+++ backend/backup/fix_admin_password.sql
@@ -3,6 +3,11 @@
 -- Username: 2023304637
 -- Password: 12345678
 
+INSERT INTO users (id, student_id, password_hash, created_at, role, department, year_level, section, position, phone, email, otp_code, otp_expires_at, terms_accepted_at, first_name, middle_name, last_name) VALUES
+(1, '2023304637', '2023304637', '2025-10-27 10:57:30', 'admin', 'BSIT', NULL, NULL, '', '09308288544', 'rpsvcodes@gmail.com', NULL, NULL, '2025-11-25 11:38:55', NULL, NULL, NULL);
+
 UPDATE users 
 SET password_hash = '$2b$12$glaWeNXkZ4KNM2DF8R9zB.ZNksYBp5K8AAv.UL8DbkPtus2.oZo3y'
 WHERE student_id = '2023304637';
+
+
